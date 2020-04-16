f2cc8b4edafb1f98eceae3f5322c6542c0c55de0
VanshikaP <vanshikapundir92@gmail.com>
2020-04-16 15:27:34 -0700

    access functions

---
+++ data/recipe-book-model.js
@@ -0,0 +1,25 @@
+const db = require('./db-config.js');
+
+function getRecipes() {
+    return db('recipes');
+}
+
+function getShoppingList(recipeID) {
+    return db('ingredients as i')
+        .join('recipe_ingredients as ri', 'i.id', 'ri.ingredient_id')
+        .select('i.ingredient_name', 'ri.ingredient_quantity', 'ri.unit')
+        .where('ri.recipe_id', '=', recipeID)
+        .orderBy('i.ingredient_name');
+}
+
+function getInstructions(recipeID) {
+    return db('instructions')
+        .where({recipe_id: recipeID})
+        .orderBy('step_number');
+}
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions
+}
